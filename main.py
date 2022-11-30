cd45e3aef75a6fbbbe0f12916013e30fcd450ca0
GSentientWolf <10687820+GSentientWolf@users.noreply.github.com>
2022-11-30 11:14:12 -0600

    95 % completed. Issues with the Respositories
    No time to complete the pydantic ORM for the json file for each repository.
    File repository is not creting the file. Missing some extra time.

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
-from logging.logging import Logging
-from sensors.base_sensor import BaseSensor
-from service.repository.repository import Repository
+
+"""
+  Demonstration main file
+"""
+
+import time
+from logger.logger import MyLogger
+from sensors.sensor import get_sensor, SensorType
+from service.repository.repository import FileRepository
 from utils.network import Network
 
 if __name__ == '__main__':
-    repository = Repository()
+
+    logging = MyLogger()
     network = Network()
-    sensors = BaseSensor(network)
-    logging = Logging(repository, network)
+    repository = FileRepository(location='./file_rep.txt', network=network)
+    thermal_sensor_05 = get_sensor(sensor_type=SensorType.THERMAL,
+                                   sensor_name='Thermal Sensor AZ01',
+                                   network=network,
+                                   exec_interval=3.2)
+    vibration_sensor_02 = get_sensor(sensor_type=SensorType.VIBRATION,
+                                     sensor_name='Vibration Sensor AZ01',
+                                     network=network,
+                                     exec_interval=1.2)
+    pressure_sensor_99 = get_sensor(sensor_type=SensorType.PRESSURE,
+                                    sensor_name='Pressure Sensor AZ01',
+                                    network=network,
+                                    exec_interval=4.6)
+    magnetic_sensor_11 = get_sensor(sensor_type=SensorType.THERMAL,
+                                    sensor_name='Magnetic Sensor AZ01',
+                                    network=network,
+                                    exec_interval=1)
+    humidity_sensor = get_sensor(sensor_type=SensorType.THERMAL,
+                                 sensor_name='Humidity Sensor AZ01',
+                                 network=network,
+                                 exec_interval=2.8)
+    network.start_network()
+    logging.add_sensors([
+        thermal_sensor_05,
+        pressure_sensor_99,
+        humidity_sensor,
+        magnetic_sensor_11,
+        vibration_sensor_02
+    ])
+    logging.start_bank()
+    time.sleep(40)
+    logging.stop_bank()
+    network.stop_network()
